658d3cbf94be43895d38ccc1051a6e2ab2d0865a
sinnisingla <sinnisingla@outlook.com>
2022-04-20 12:12:39 +0530

    feat: add post and put apis

--- BooksApiWithEF/Controllers/BooksController.cs
+++ BooksApiWithEF/Controllers/BooksController.cs
@@ -22,5 +22,30 @@ namespace BooksApiWithEF.Controllers
         {
             return _context.Books.ToList();
         }
+
+        [HttpGet]
+        [Route("/seatch/{searchText}")]
+        public List<Book> SearchBooksByTitle(string searchText) 
+        {
+            return _context.Books.Where(book => book.Title.ToLower().Contains(searchText)).ToList();
+        }
+
+        [HttpPost]
+        public Book AddNewBook(Book newbook)
+        {
+            _context.Books.Add(newbook);
+            _context.SaveChanges();
+            var addedBook = _context.Books.Where(book => book.Title.Contains(newbook.Title)).FirstOrDefault();
+            return addedBook;
+        }
+
+        [HttpPut]
+        public Book updateBook(Book updatedBook)
+        {
+            _context.Books.Update(updatedBook);
+            _context.SaveChanges();
+            var book = _context.Books.Where(book => book.BookId == updatedBook.BookId).FirstOrDefault();
+            return book;
+        }
     }
 }
