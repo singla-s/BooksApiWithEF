3351b8d987f5af5bb51f7adb899681d5ab439ea7
sinnisingla <sinnisingla@outlook.com>
2022-04-20 11:45:13 +0530

    feat: add get all books

---
+++ BooksApiWithEF/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using BooksApiWithEF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BooksApiWithEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksDbContext _context;
+
+        public BooksController(BooksDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+    }
+}
